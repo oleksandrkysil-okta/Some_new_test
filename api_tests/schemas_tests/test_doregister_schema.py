64b31bb8d4f45fd3393c6cb28a293dceca741438
AlexKysil <alex.kisel0504@gmail.com>
2020-02-17 22:39:19 +0200

    add requirements, added allure features tag to test

--- api_tests/schemas_tests/test_doregister_schema.py
+++ api_tests/schemas_tests/test_doregister_schema.py
@@ -1,11 +1,13 @@
 import requests
 import pytest
+import allure
 
 from api_tests.conftest import validator
 from global_helpers.constants import REQUEST_LINK
 from global_helpers.helpers import email_name_generator
 
-
+@allure.feature("User Registration API")
+@allure.story("Validate registration schema")
 @pytest.mark.NW_schema_test
 def test_doregister_schema():
     """
